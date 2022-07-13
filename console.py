4ffed67cdb332367221004de314027795c2ee0ec
CarlyCoding <carly_off@outlook.com>
2022-07-13 15:04:48 +0100

    added first author

--- console.py
+++ console.py
@@ -2,3 +2,9 @@ import pdb
 from models.author import Author
 from models.book import Book
 
+import repositories.author_repository as author_repository
+
+
+author1 = Author("Bill")
+author_repository.save(author1)
+
